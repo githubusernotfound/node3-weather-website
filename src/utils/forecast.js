2bba220f1061aefadbb69a4b399de5d3869cca87
githubusernotfound <blagaj100@gmail.com>
2019-08-10 00:32:30 +0200

    Add leng en.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude,longitude, callback) => {
-    const url = "https://api.darksky.net/forecast/167b7c2802450bc7c2b46d021c432014/37.8267,-122.4233?lang=bs";
+    const url = "https://api.darksky.net/forecast/167b7c2802450bc7c2b46d021c432014/37.8267,-122.4233";
 
     request({ url, json: true}, (error, { body })=>{
         if(error) {
